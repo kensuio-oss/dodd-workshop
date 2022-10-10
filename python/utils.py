83d6e76bd5e5ff56bd112c9463de7bdcb35daf4e
Leszek Gruchała <leszek.gruchala@kensu.io>
2022-10-10 18:22:14 +0200

    Prepare the code for the new major releases of KensuPy and Spark-Collector

--- python/utils.py
+++ python/utils.py
@@ -10,7 +10,7 @@ user = ""
 project = ""
 env = ""
 
-KensuProvider().initKensu(api_url=ingestion_url, auth_token=ingestion_token, process_name= process,
+KensuProvider().initKensu(kensu_ingestion_url=ingestion_url, kensu_ingestion_token=ingestion_token, process_name= process,
                             user_name= user, code_location='https://gitlab.example.com',
-                            project_names=[project], environment= env, pandas_support=True, sklearn_support=False,
+                            project_name=project, environment= env, pandas_support=True, sklearn_support=False,
                                 tensorflow_support=False, bigquery_support=False,pyspark_support=False)
