aeffb326feb7c5bd6b6200960149d0b75146bb5b
Leszek Gruchała <leszek.gruchala@kensu.io>
2022-10-25 09:42:44 +0200

    Prepare the code for the new major releases of KensuPy and Spark-Collector (#2)

--- spark/utils.py
+++ spark/utils.py
@@ -3,67 +3,67 @@ import shutil
 
 
 
-def create_properties_files(trainee, api_url = "", ingestion_token = "",pat="", sdk_url ="", offline= False):
+def create_properties_files(trainee, ingestion_url ="", ingestion_token ="", api_token="", api_url ="", offline= False):
     
-    if not offline and (api_url == "" or ingestion_token == ""):
-        raise Exception("Missing api_url or token")
+    if not offline and (ingestion_url == "" or ingestion_token == ""):
+        raise Exception("Missing ingestion_url or ingestion_token")
         
     
-    if (api_url == "" and ingestion_token != "") or (api_url != "" and ingestion_token == ""):
-        raise Exception("Missing PAT or SDK_URL")
+    if (ingestion_url == "" and ingestion_token != "") or (ingestion_url != "" and ingestion_token == ""):
+        raise Exception("Missing ingestion or api configuration")
     
         
 
     prop = """
     
-    dam.activity.user=%s
+    user_name=%s
     
-    dam.ingestion.url=%s
-    dam.ingestion.auth.token=%s
+    kensu_ingestion_url=%s
+    kensu_ingestion_token=%s
     
     kensu.pat=%s
     kensu.sdk.url=%s
     
-    dam.activity.environment=workshop
-    dam.activity.projects=%s_dodd
-    dam.activity.explicit.process.name=%s
-    dam.activity.explicit.process_run.name=%s
-    dam.activity.code.repository=binder://workshop-dodd
-    dam.activity.code.version=v1
-    dam.activity.organization=Unknown
+    environment=workshop
+    project_name=%s_dodd
+    process_name=%s
+    process_run_name=%s
+    code_location=binder://workshop-dodd
+    code_version=v1
 
-    mocked.timestamp=%d
+    execution_timestamp=%d
 
-    dam.datasources.short.name=true
-    dam.datasources.path_rules.short.naming_strategy=
-    dam.datasources.short.naming_strategy=File
-    dam.logical.datasources.path_rules.naming_strategy=
-    dam.logical.datasources.naming_strategy=File
+    shorten_data_source_names=true
+    data_source_naming_strategy_rules=
+    data_source_naming_strategy=File
+    logical_data_source_naming_strategy_rules=
+    logical_data_source_naming_strategy=File
 
-    dam.spark.data_stats.input.computeQuantiles=false
-    dam.spark.data_stats.input.cachePerPath=true
-    dam.spark.data_stats.input.coalesceEnabled=true
-    dam.spark.data_stats.input.coalesceWorkers=1
-    dam.spark.data_stats.output.computeQuantiles=false
-    dam.spark.data_stats.output.cachePerPath=false
-    dam.spark.data_stats.output.coalesceEnabled=false
-    dam.spark.data_stats.output.coalesceWorkers=1
+    input_stats_compute_quantiles=false
+    input_stats_compute_quantiles=false
+    input_stats_cache_by_path=true
+    input_stats_coalesce_enabled=true
+    input_stats_coalesce_workers=1
+    output_stats_compute_quantiles=false
+    output_stats_cache_by_path=false
+    output_stats_coalesce_enabled=false
+    output_stats_coalesce_workers=1
 
-    dam.ingestion.is_offline=%s
-    dam.ingestion.offline.file=entities.log
-    dam.ingestion.ignore.ssl.cert=true
-    dam.ingestion.entity.compaction=true
-    dam.spark.data_stats.enabled=true
-    dam.spark.data_stats.input.enabled=true
-    dam.spark.data_stats.input.only_used_in_lineage=true
-    dam.spark.data_stats.input.keep_filters=false
-    dam.spark.shutdown_timeout=600
+    report_to_file=%s
+    offline_file_name=entities.log
+    kensu_api_verify_ssl=false
+    enable_entity_compaction=true
+    compute_stats=true
+    compute_input_stats=true
+    input_stats_fallback_to_all_columns_if_not_present_in_lineage=true
+    input_stats_for_only_used_columns=false
+    shutdown_timeout_sec=600
 
-    dam.spark.file_debug.level=INFO
-    dam.spark.file_debug.file_name=debug.log
-    dam.spark.file_debug.capture_spark_logs=false
+    collector_log_level=INFO
+    collector_log_file_path=debug.log
+    collector_log_include_spark_logs=false
 
-    dam.activity.spark.environnement.provider=io.kensu.dam.lineage.spark.lineage.DefaultSparkEnvironnementProvider
+    spark_environment_provider=io.kensu.sparkcollector.system.DefaultSparkEnvironmentProvider
     """
 
 
@@ -72,7 +72,7 @@ def create_properties_files(trainee, api_url = "", ingestion_token = "",pat="", 
     i = 0
     for app in apps:
         for week in weeks:
-            p = prop % (trainee, api_url, ingestion_token, pat, sdk_url, trainee, app, app+trainee, week, str(offline).lower())
+            p = prop % (trainee, ingestion_url, ingestion_token, api_token, api_url, trainee, app, app + trainee, week, str(offline).lower())
             with open("conf/application%d-week%d.properties" % (apps.index(app)+1, weeks.index(week)+1), "w") as f:
                 f.write(p)
 
